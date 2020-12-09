db1a08104709d09dd8272aa301d05b984b2208af
lindalevy <lglevy@bigpond.net.au>
2020-12-09 17:53:32 +1100

    final update of working exercise

--- PyPoll/analysis/main.py
+++ PyPoll/analysis/main.py
@@ -11,9 +11,11 @@ import csv
 csvpath = os.path.join('..', 'Resources', 'election_data.csv')
 
 # create lists to use in manipulation to produce outputs
-listvotes = list()   
-candidate_options = list()
-outputdir = {}
+listvotes = list()  # a full list of each vote, storing just the candidate name  
+candidate_options = list() #list of unique candidates
+
+outputdir = {}  #unique candidates with their sum of votes
+candidatepercentlist = list() #list of the percentage of votes per unique candidate
 
 with open(csvpath, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -32,12 +34,13 @@ with open(csvpath, "r") as csvfile:
         listvotes.append(rowcandidate)
         totalvotes = len(listvotes)
         
-          # (In a way, our loop is "discovering" candidates as it goes)
+        # (our loop is "discovering" candidates as it goes)
         if rowcandidate not in candidate_options:
             # Add it to the list of candidates in the running
             candidate_options.append(rowcandidate)
             # And begin tracking that candidate's voter count
             outputdir[rowcandidate] = 0
+            localcount =+ 1
 
         # Then add a vote to that candidate's count
         outputdir[rowcandidate] = outputdir[rowcandidate] + 1
@@ -45,27 +48,35 @@ with open(csvpath, "r") as csvfile:
     # find winner 
     winner = max(outputdir, key= outputdir.get)
 
-    #time to create a file and all outcomes to the text file
+    #what is the number of unique candidates
+    unique = len(outputdir)
+    unique = int(unique)
+
+    #can we get lists, to calculate the percentage
+    # first create 2 lists from the directory
+    outputvalueslist = list(outputdir.values())
+    outputdirkeylist = list(outputdir.keys())
+
+    # calculate percentages, creating another list (candidate percentlist)
+    for i in range(0, unique):
+        percent = '{:.2%}'.format((outputvalueslist[i] / totalvotes))
+        candidatepercentlist.append(percent)
+    
+    #time to create a file and write all outcomes to the text file
     newfile= open("myPYPollResults.txt", "w")
     newfile.write("Election Results\n" + "--------------------\n")
     newfile.write(f"Total Votes: {totalvotes}\n")
     newfile.write("--------------------\n")
-    newfile.write( "   ")
+    newfile.write("   ")
     newfile.write(f"Winner: {winner}\n")
-    
     newfile.write( "   ")
     newfile.write("--------------------\n")
-    newfile.writelines('{}:{}\n'.format(k,v) for k, v in outputdir.items()) 
+    for i in range(0, unique):
+       newfile. write(f"    {outputdirkeylist[i]}    {candidatepercentlist[i]}   ({outputvalueslist[i]})\n")
     newfile.write("--------------------------")
-
     newfile.close()
  
-    #start printing, and put spacing between to make it more readable
-    # use this in the print function
-    #        vpercent = "{:.3%}".format(vpercent)
-
-    ## I have been unable to figure out in the time how to manage the percentages so this is not included
-
+    # print it out and go Home!
     print ("    ")
     print("Election Results")
     print("----------------")
@@ -75,9 +86,10 @@ with open(csvpath, "r") as csvfile:
     print( "   ")
 #   print winner
     print(f"Winner: {winner}")
+    print( "   ")
     print("--------------------------")
-    for key, value in outputdir.items():
-        print(key, ' : ', value)
-
+    #print individual candiates results
+    for i in range(0, unique):
+        print(f"    {outputdirkeylist[i]}   {candidatepercentlist[i]}   ({outputvalueslist[i]}) ")
     print( "   ")
     print("--------------------------")
